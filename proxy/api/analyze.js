f86774d5b8bb6caedd27cbec29c6b97f8e35acfb
jungin <changju@bu.edu>
2026-01-30 16:18:21 -0600

    fix build error

--- proxy/api/analyze.js
+++ proxy/api/analyze.js
@@ -1,5 +1,5 @@
 // proxy/api/analyze.js
-import { getAnalysisPrompt } from '../utils/prompts';
+import { getAnalysisPrompt } from '../utils/prompts.js';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
